1d84ce59562bd8e1190fc3390148d5fe9e149686
Anton Prokopenko <anton.9090@bk.ru>
2017-11-24 15:31:52 +0300

    Fix localhost

--- server.js
+++ server.js
@@ -6,15 +6,14 @@ const PORT = process.env.PORT || 3000;
 
 //this is for Heroku
 app.use(function(req, res, next) {
-  if(req.headers['x-forwarded-proto'] === 'http') {
-    next();
+  if(req.headers['x-forwarded-proto'] === 'https') {
+    res.redirect('http://' + req.hostname + req.url);    
   } else {
-    res.redirect('http://' + req.hostname + req.url);
+    next();
   }
 });
 
 app.use(express.static('public'));
-
 
 app.listen(PORT, function () {
   console.log('Server Is Working Now on ' + PORT);
