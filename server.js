45f2f2898fedf0e906889ecd138b5390e8b04432
Anton Prokopenko <anton.9090@bk.ru>
2017-11-24 11:52:03 +0300

    Prep repo for Heroku

--- server.js
+++ server.js
@@ -2,7 +2,20 @@ var express = require('express');
 
 //Create our app
 var app = express();
+const PORT = process.env.PORT || 3000;
+
+//this is for Heroku
+app.use(function(req, res, next) {
+  if(req.headers['x-forwarded-proto'] === 'http') {
+    next();
+  } else {
+    res.redirect('http://' + req.hostname + req.url);
+  }
+});
+
 app.use(express.static('public'));
-app.listen(3000, function () {
-  console.log('Server Is Working Now');
+
+
+app.listen(PORT, function () {
+  console.log('Server Is Working Now on ' + PORT);
 });
